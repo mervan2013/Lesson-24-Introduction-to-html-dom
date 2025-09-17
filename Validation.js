bec319554d3fde88f9f7f737e3391140b01fa41a
mervan2013 <mervansivarooban2013@gmail.com>
2025-09-17 16:07:28 -0700

    h

---
+++ Validation.js
@@ -0,0 +1,14 @@
+function validate(e) {
+    const email=document.getElementById('email').value;
+    const pass=document.getElementById('password').value;
+    const age=document.getElementById('age').value;
+    const msgBOX=document.getElementById('message');
+
+    let message='';
+
+    id (email === '') {
+        message='enter a Email.';
+        msgBOX
+    }
+
+}
